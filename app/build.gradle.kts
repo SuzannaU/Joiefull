634672f1f1272401ad8fc573f5abb2515944e054
SuzannaU <suzanna.ursulet@gmail.com>
2026-04-03 18:14:52 -0400

    Addition of libraries and plugins for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,14 +41,48 @@ android {
 }
 
 dependencies {
+    // Kotlin
     implementation(libs.androidx.core.ktx)
+    runtimeOnly(libs.coroutines.core)
+    runtimeOnly(libs.coroutines.android)
+    testImplementation(libs.coroutines.test)
+
+    // Lifecycle
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Activity
     implementation(libs.androidx.activity.compose)
+
+    // Navigation
+    implementation(libs.androidx.navigation)
+
+    // UI
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
+
+    // Koin
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    testImplementation(libs.koin.test)
+    testImplementation(libs.koin.test.junit5)
+
+    // Retrofit - Moshi
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+
+    // Coil
+    implementation(libs.coil)
+    implementation(libs.coil.network)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
